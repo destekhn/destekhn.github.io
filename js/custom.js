689b071b3c7e7f741c69f2f1523e5d9c83b47bf9
osman hernandez <osman.sist@gmail.com>
2021-09-11 15:44:05 +0200

    destek html

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	//smoothScroll
+	  smoothScroll.init();
+	  //Scroll Spy
+	  $('body').scrollspy({ target: '.navbar' })
+	//0wl-caurosel
+	  $(".owl-carousel").owlCarousel(
+	  {
+		loop:true,
+		margin:5,
+		center:true,
+		nav:false,
+		dot:true,
+		lazyload:true,
+		slideSpeed:5000,
+		paginationSpeed:5000,
+		rewindSpeed:5000,
+		navigation :true,
+		pagination:true,
+		autoplay:true,
+		autoplaySpeed:2000,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	}
+	  );
+	//video popup
+    $('.all-video-btn a').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>',
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+                }
+            },
+            srcAction: 'iframe_src'
+        }
+    });
+	//slick slider
+	$('.item').slick({
+	  dots: true,
+	  speed:2000,
+	  slidesToShow:1,
+	  autoplay: true,
+	  autoplaySpeed:2500,
+	  adaptiveHeight: false
+	});
+});
